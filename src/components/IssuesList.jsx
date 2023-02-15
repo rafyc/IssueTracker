42bec0d74472ad592aac247df8c14cf625f50e92
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-02-15 17:00:15 -0500

    + recoil data fromreactQuery

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
+  const issueQuerry = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
+  console.log(issueQuerry.data);
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issueQuerry.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {issueQuerry.data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentBy={issue.commentBy}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+              commentCount={issue.comments.length}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
